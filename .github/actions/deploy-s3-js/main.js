6558309f48f5a590b51af5db28af446269a8d117
Manuel Ramirez <manuel.ramirez@siriusxm.com>
2026-08-24 13:54:39 -0500

    added custom deploy action

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -3,6 +3,18 @@ import * as github from '@actions/github'
 import * as exec from '@actions/exec'
 
 function run() {
+
+    // get inputs
+    const bucket = core.getInput('bucket', {required: true})
+    const bucketRegion = core.getInput('bucket-region', {required: true})
+    const distFolder = core.getInput('dist-folder', {required: true})
+
+    //github.context.
+    // upload files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
+
     core.notice('hello from js action');
 }
 
